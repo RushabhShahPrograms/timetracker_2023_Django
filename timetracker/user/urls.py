f9dbd1c05ba60e2ac75e9fa9d4e5126f3119ba86
Rushabh Shah <rushabhshah122000@gmail.com>
2023-04-09 20:15:29 +0530

    reset password 🍉

--- timetracker/user/urls.py
+++ timetracker/user/urls.py
@@ -1,16 +1,25 @@
 from . import views
 from django.urls import path,include
 from .views import *
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
- 
+ #registerURLS
  path('adminregister/',AdminRegisterView.as_view(),name='adminregister'),
  path('managerregister/',ManagerRegisterView.as_view(),name='managerregister'),
  path('developerregister/',DeveloperRegisterView.as_view(),name='developerregister'),
+ #loginlogoutURLS
  path('login/',UserLoginView.as_view(),name='login'),
  path('logout/',views.logoutUser,name='logout'),
+ #sendmailURLS
  path('sendmail/',views.sendMail,name='sendmail'),
+ #dashboardpageURLS
  path('managerpage/',ManagerPage.as_view(),name="managerpage"),
  path('developerpage/',DeveloperPage.as_view(),name="developerpage"),
  path('adminpage/',AdminPage.as_view(),name="adminpage"),
+ #passwordresetURLS
+ path('resetpassword/',auth_views.PasswordResetView.as_view(),name='reset_password'),
+ path('resetpasswordsent/',auth_views.PasswordResetViewDoneView.as_view(),name='password_reset_done'),
+ path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+ path('resetpasswordcomplete/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
 ]
