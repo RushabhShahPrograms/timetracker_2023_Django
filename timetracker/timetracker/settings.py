9008f842ab7bc6cabce0c6f1b15c99d83d1ace22
Rushabh Shah <90546286+RushabhShahPrograms@users.noreply.github.com>
2023-05-24 10:04:46 +0530

    Update settings.py

--- timetracker/timetracker/settings.py
+++ timetracker/timetracker/settings.py
@@ -157,5 +157,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587 
-EMAIL_HOST_USER = 'bloggerrushabhshah@gmail.com'
-EMAIL_HOST_PASSWORD = 'lvxdmtrldtllcsog'
+EMAIL_HOST_USER = 'emailId'
+EMAIL_HOST_PASSWORD = 'password'
